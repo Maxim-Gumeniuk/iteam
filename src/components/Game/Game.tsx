49c9212cf66b93c9ba6a891faf4c1ac3540e18a1
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-01-14 10:46:19 +0200

    event was added

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -14,11 +14,14 @@ export const Game: React.FC<Props> = ({ game }) => {
   const [like, setLike] = useState(false);
   const dispatch = useAppDispatch();
 
-  const addGame = (game: Games) => {
+  const addGame = (game: Games, event:React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    event.preventDefault();
+
     dispatch(LikedActions.addGame(game));
   }
 
-  const removeGame = (game: Games, id:number) => {
+  const removeGame = (game: Games, id:number, event: React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+    event.preventDefault();
     dispatch(LikedActions.removeGame(game));
     dispatch(actions.setId(id));
   }
@@ -31,39 +34,39 @@ export const Game: React.FC<Props> = ({ game }) => {
   const handleLIke = () => {
     setLike(prev => !prev);
   };
+
   return (
     <div className={styles.card}>
       <Link to='/details' onClick={() => addId(+game.appId)}>
-      <img
-        className={styles.image}
-        src={game.imgUrl}
-        alt="game"
-      />
-      { like && (
-        <div className={styles.box}>
-          <div className={styles.border}>
-            <img
-              className={styles.play}
-              src={require('../../images/play-icon.png')} 
-              alt="play" 
-            />
+        <img
+          className={styles.image}
+          src={game.imgUrl}
+          alt="game"
+        />
+        { like && (
+          <div className={styles.box}>
+            <div className={styles.border}>
+              <img
+                className={styles.play}
+                src={require('../../images/play-icon.png')} 
+                alt="play" 
+              />
+            </div>
           </div>
-        </div>
         )}
-      </Link>
-        <div className={styles.body}>
-          <p className={styles.title}>{game.title}</p>
-          <p  className={styles.released}>{game.released}</p>
-          <div className={styles.row}>
-            <p>{game.price}</p>
-            <div onClick={() => handleLIke()}>
+          <div className={styles.body}>
+            <p className={styles.title}>{game.title}</p>
+            <p  className={styles.released}>{game.released}</p>
+            <div className={styles.row}>
+              <p>{game.price}</p>
+              <div onClick={() => handleLIke()}>
               {
                 like ? (
                   <img
                     className={styles.heart}
                     src={require('../../images/red-heart.png')}
                     alt="heart"
-                    onClick={() => removeGame(game, +game.appId)}
+                    onClick={(e) => removeGame(game, +game.appId, e)}
                   />
                 )
                 : (
@@ -71,13 +74,14 @@ export const Game: React.FC<Props> = ({ game }) => {
                   className={styles.heart}
                   src={require('../../images/white-heart.png')}
                   alt="heart"
-                  onClick={() => addGame(game)}
+                  onClick={(e) => addGame(game, e)}
                 />
               )
-            }
+              }
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
